dfd8a34187437f6a143b0927d954f10ce65c018a
K.Saikrishna <108404892+Chem2527@users.noreply.github.com>
2025-01-06 14:32:54 +0530

    Update metabase.conf under conf.d

--- metabase.conf under conf.d
+++ metabase.conf under conf.d
@@ -1,12 +1,6 @@
-
-cd conf.d/
-Created a file inside conf.d with name metabase.conf
-root@ip-172-31-25-172:/etc/nginx/conf.d# ls
-metabase.conf
-root@ip-172-31-25-172:/etc/nginx/conf.d# cat metabase.conf
 server {
     listen 80;
-    server_name 3.91.66.18;  # Replace with your EC2's public IP or domain name
+    server_name 13.201.58.50;  # Replace with your EC2's public IP or domain name
 
     # Reverse Proxy to sai container (port 3010) for /dev
     location /dev/ {
@@ -25,6 +19,19 @@ server {
     location /test/ {
         rewrite ^/test(/.*)$ $1 break;  # Strips /test prefix
         proxy_pass http://127.0.0.1:3011;  # sai1 container's port
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_connect_timeout 10s;  # Set a connection timeout
+        proxy_read_timeout 60s;     # Set a read timeout
+        proxy_send_timeout 60s;     # Set a send timeout
+    }
+
+    # Reverse Proxy to metabase_prod container (port 3012) for /production
+    location /production/ {
+        rewrite ^/production(/.*)$ $1 break;  # Strips /production prefix
+        proxy_pass http://127.0.0.1:3012;  # metabase_prod container's port
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
